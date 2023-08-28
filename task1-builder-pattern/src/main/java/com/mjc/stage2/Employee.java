0edde85b078b1582b0fb835544456ec68558b5ce
yuliyago <yuuliya02@proton.me>
2023-08-29 00:25:21 +0300

    Creational Design Patterns tasks were done.

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -70,5 +70,44 @@ public class Employee {
 
     public static class EmployeeBuilder {
         // Write your code here!
+        private Employee employee;
+
+        public EmployeeBuilder() {
+            employee = new Employee();
+        }
+
+        public EmployeeBuilder setName(String name) {
+            employee.setName(name);
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            employee.setLastName(lastName);
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            employee.setPosition(position);
+            return this;
+        }
+
+        public EmployeeBuilder setPhone(String phone) {
+            employee.setPhone(phone);
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            employee.setEmail(email);
+            return this;
+        }
+
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            employee.setCarNumber(carNumber);
+            return this;
+        }
+
+        public Employee build() {
+            return employee;
+        }
     }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,31 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
     // Write your code here!
+    private static final String JDBC_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:~/test";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    @Override
+    public Connection createConnection() {
+        try {
+            Class.forName(JDBC_DRIVER);
+            Properties properties = new Properties();
+            properties.setProperty("user", USER);
+            properties.setProperty("password", PASSWORD);
+            return DriverManager.getConnection(DB_URL, properties);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
 
